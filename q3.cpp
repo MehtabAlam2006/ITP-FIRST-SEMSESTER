f1dae8338867079f8a6d14d20cdf420e2a49d61b
Mehtab Alam <138979331+MehtabAlam2006@users.noreply.github.com>
2024-08-25 12:37:42 +0500

    Question 3 done

---
+++ q3.cpp
@@ -0,0 +1,17 @@
+/*Write a program that creates and displays a table consisting of a list of students and their ITP scores in
+a tabular format. The program should use \t for proper alignment and \n to move between rows. You
+need to display at least three entries in the table. Note: Formatting of the code is all up to you.*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"      SCORE BOARD OF ITP STUDENS      \n";
+    cout<<"---------------------------------------\n";
+    cout<<"Name:\t\t\t"<<"ITP Score\n";
+    cout<<"Alam Khan \t\t"<<"50/100\n";
+    cout<<"Hamza ALi \t\t"<<"70/100\n";
+    cout<<"Mubashir Hassan \t"<<"80/100\n";
+    cout<<"Mehtab Alam\t\t"<<"65/100\n";
+    
+    return 0;
+}

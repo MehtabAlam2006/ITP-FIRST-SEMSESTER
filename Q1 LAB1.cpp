c2c87cffce9326c6d525da63cf672e3ea62df29a
Mehtab Alam <138979331+MehtabAlam2006@users.noreply.github.com>
2024-08-25 12:18:45 +0500

    Q1 and Q2 DONE

---
+++ Q1 LAB1.cpp
@@ -0,0 +1,21 @@
+
+/*1. Write a program that prints your household grocery list consisting of as many items as you need. Use
+‘\n’ to print each item of the list on a new line.
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Household Grocery List: \n";
+    cout<<"Milk\n";
+    cout<<"Eggs\n";
+    cout<<"Bread\n";
+    cout<<"Rice\n";
+    cout<<"Apples\n";
+    cout<<"Chicken\n";
+    cout<<"Vegetables\n";
+    cout<<"Coffee\n";
+
+    return 0;
+
+}
---
+++ Q2 LAB1.cpp
@@ -0,0 +1,15 @@
+
+/*Write a program that creates and displays a table with four columns: Name, Age, Gender, and Height.
+The program should use \t for proper alignment and \n to move between rows. You need to display at
+least three entries in the table.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Name \t "<<"Age \t"<<"Gender\t"<<"Height\n";
+    cout<<"Mehtab \t"<<"18 \t"<<"M \t"<<"6.4 \n";
+    cout<<"Alam \t"<<"19 \t"<<"M \t"<<"6.0 \n";
+    cout<<"Talha \t"<<"30 \t"<<"M \t"<<"5.6 \n";
+    cout<<"Azlan \t"<<"19 \t"<<"M \t"<<"6.6 \n";
+
+}

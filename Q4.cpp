f38269fbe97cef15d9a0e43bb27e92605e0c6719
Mehtab Alam <138979331+MehtabAlam2006@users.noreply.github.com>
2024-08-25 12:44:44 +0500

    Q3 and Q4 done

---
+++ Q4.cpp
@@ -0,0 +1,12 @@
+/*4. Write a program that initializes an integer variable with a user's age and then prints a message that
+includes this age. The message should be of the format: "You are [Age] years old."
+Requirements:
+• Declare an int variable and initialize it with the user's age.
+• Use cout to print a message in the format: "You are [Age] years old."*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 60;
+    cout<<"You are "<< age<< "years old\n";
+}
